700209b18093f35229346fa7cd80501fd445991d
Stephan Kulow <coolo@suse.de>
2008-04-05 07:44:01 +0000

    small tool to update all of zypp:svn

---
+++ update_zypp_repo/update_all.sh
@@ -0,0 +1,35 @@
+set -e
+homedir=$PWD
+dir=`mktemp -d`
+cd $dir
+old=`cat $homedir/last_rev`
+new=`LC_ALL=C svn info http://svn.opensuse.org/svn/zypp/trunk | grep ^Revision: | sed -e 's,.*: *,,'`
+echo $old:$new
+for pack in libsatsolver libzypp libzypp-testsuite-tools libzypp-bindings zypper; do
+  
+  case $pack in
+    libsatsolver)
+      repo=sat-solver
+      ;;
+    libzypp-testsuite-tools)
+      repo=libzypp-testsuite
+      ;;
+    *)
+      repo=$pack
+  esac 
+
+  url=http://svn.opensuse.org/svn/zypp/trunk/$repo
+  log=`svn log -r$old:$new $url | grep ^r` || true
+  test -n "$log" || continue
+
+  osc co zypp:svn $pack
+  cd zypp:svn/$pack
+
+  svn cat $url/package/update_rpm | sh
+  osc addremove
+  osc ci
+done
+cd /
+rm -rf $dir
+echo $new > $homedir/last_rev
+
